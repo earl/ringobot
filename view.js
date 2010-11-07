57a81900f1d8f39a6529e2ca601377a42854c6c1
Hannes Wallnöfer <hannesw@gmail.com>
2010-11-07 16:26:22 +0100

    Smarter script rendering

--- view.js
+++ view.js
@@ -1,16 +1,22 @@
 var {html4, html, css} = require("static/hiccup");
 
+var scripts = [
+    "/static/org/cometd.js",
+    "/static/jquery/jquery-1.3.2.js",
+    "/static/jquery/jquery.json-2.2.js",
+    "/static/jquery/jquery.cometd.js",
+    "/static/hiccup.js",
+    "/static/app.js"
+].map(function(script) {
+    return ["script", {type:"text/javascript", src: script}];
+});
+
 exports.page = function(day, records, days) {
     return html4(
       ["head",
         ["title", "RingoBot IRC Logs"],
         ["link", {rel: "stylesheet", type: "text/css", href: "/static/style.css"}],
-        ["script", {type:"text/javascript", src: "/static/org/cometd.js"}],
-        ["script", {type:"text/javascript", src: "/static/jquery/jquery-1.3.2.js"}],
-        ["script", {type:"text/javascript", src: "/static/jquery/jquery.json-2.2.js"}],
-        ["script", {type:"text/javascript", src: "/static/jquery/jquery.cometd.js"}],
-        ["script", {type:"text/javascript", src: "/static/hiccup.js"}],
-        ["script", {type:"text/javascript", src: "/static/app.js"}],
+        scripts,
         ["style",  {type: "text/css"},
             css("p.utterance", {margin: 0},
                 "span.is_action", {fontStyle: "italic"})]
