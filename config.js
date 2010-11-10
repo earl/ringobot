da3f054a44f705292b573ddc4088ad82c819384e
Hannes Wallnöfer <hannesw@gmail.com>
2010-11-10 18:38:32 +0100

    Fix github webhook URL and channel

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ exports.botConfig = {
 };
 
 exports.webhookConfig = {
-    github: {repositories: {'https://github.com/user/repo': '##test'}},
+    github: {repositories: {'https://github.com/ringo/ringojs': '#ringojs'}},
 };
 
 // --- general webapp config ---
